0b61c97740eb86cbce3266b7d803ee2d698dc449
Pratipada <pratipadakhatode@gmail.com>
2024-06-03 16:48:29 +0530

    004

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Accordion from './Accordion'
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      {/*<Accordion />*/}
+      <Sidebar />
        {/* Correctly render the RandomColor component */}
     </div>
   );
